5d8ca10c7aac4f5ad039475bbdabbdc349b06be4
MateoEnterprises <mat.thompson02@gmail.com>
2015-03-15 18:51:44 -0600

    adding up to date project work

--- ecma/app.js
+++ ecma/app.js
@@ -3,16 +3,25 @@ var app = angular.module('devmtnApp', []);
 app.controller('devCont', function ($scope, fourSquareAPI) {
   
   $scope.test = 'test';
+  $scope.getActivity = false;
   
 //  *****Gets data from FourSquare*****
   $scope.queryFourSquare = function () {
-    fourSquareAPI.explore($scope.location, $scope.distance)
-      .then(function(response){
-        $scope.parsedData = response; //this is an array of data
-      
-      })
+    fourSquareAPI.explore($scope.location, $scope.distance, $scope.price)
+    .then(function(response){
+      $scope.establishments = response; //this is an array of data
+//      var randomNumber = ;
+      $scope.randomEstablishment = $scope.establishments[Math.floor(Math.random() * ($scope.establishments.length))];
+    })
     
+    if ($scope.getActivity) {
+      fourSquareAPI.activity($scope.location, $scope.distance, $scope.activity)
+      .then(function(response){
+      $scope.activities = response; //this is an array of data
+      $scope.randomActivity = $scope.activities[Math.floor(Math.random() * ($scope.activities.length))];
+      console.log($scope.randomActivity.name)
+      })
+    }
   }
-  
   
 });
--- ecma/factory.js
+++ ecma/factory.js
@@ -2,20 +2,33 @@ var app = angular.module('devmtnApp');
 
 app.service('fourSquareAPI', function ($http, $q) {
 
-//  *****get data from foursquare api*****
-  this.explore = function (location, distance) {
+//  *****get establishment from foursquare api*****
+  this.explore = function (location, distance, price) {
     var deffered = $q.defer();
+    var milesToMeters = distance * 1609.34;
     return $http.get(
-//      "https://api.foursquare.com/v2/venues/explore?near=" + location + "&section=food"+ "&radius=" + distance + "client_id=KQUHSKE204IWZWEUQOSQBAHTB5EQZLIJ0MNEZMXVPYZTEZI5&client_secret=BTDWPJFAPDKNWXBIGNXVEILSKGO2E2PZEWOX1AJWVD5TDSDJ&v=20150101"
-      "https://api.foursquare.com/v2/venues/explore?limit=50&near=" + location + "&section=restaurant,topPicks&raduis=" + distance + "&client_id=KQUHSKE204IWZWEUQOSQBAHTB5EQZLIJ0MNEZPYZTEZI5&client_secret=BTDWPJFAPDKNWXVEILSKGO2E2PZEWOX1AJWVD5TDSDJ&v=20150312"
+      "https://api.foursquare.com/v2/venues/explore?limit=50&near=" + location + "&categoryId=4d4b7105d754a06374d81259&price=1," + price +  "&radius=" + milesToMeters + "&venuePhotos=1&client_id=&client_secret=&v=20150312"
     )
     .then(function(response) {
-      console.log(response);
       deffered.resolve(response.data.response.groups[0].items)
       return deffered.promise
     })
   }
 
+//  ***** get activity information *****
+  this.activity = function (location, distance, activity) {
+    var deffered = $q.defer();
+    var milesToMeters = distance * 1609.34;
+    return $http.get(
+      "https://api.foursquare.com/v2/venues/search?limit=50&near=" + location + "&categoryId=" + activity + "&radius=" + milesToMeters + "&venuePhotos=1&client_id=&client_secret=&v=20150312"
+    )
+    .then(function(response) {
+      console.log(response);
+      deffered.resolve(response.data.response.venues);
+      return deffered.promise;
+    })
+  }
+  
 })
 
 
